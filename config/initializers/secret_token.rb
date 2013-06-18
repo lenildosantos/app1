78f5bb578b2a98ab8d5de0e400dd08532af0cc2c
root <root@almada.securemailmarketing.com>
2013-06-18 01:41:23 +0100

    baseline

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+App1::Application.config.secret_token = '4e9fe572bbca5edeaa76b3aab6795b1f3233beacb81c056151bb47dca4919a4898a23af26cf22a5069c96befca4d60c6135521efdd4f82064e6aaf7d38ae72ff'
